0e1f7f18d9b387513ca5b5cda9ac9f4c2b2bca08
Kunjai <yadukrishnanvs.official.com>
2025-01-18 12:10:59 +0530

    supa connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:lib/pages/login.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -11,19 +12,13 @@ void main() async {
   runApp(const MainApp());
 }
 
-
-
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
