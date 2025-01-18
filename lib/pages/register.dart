6dab84522156e3d0f982478d0504444da75e22be
Kunjai <yadukrishnanvs.official.com>
2025-01-18 18:19:19 +0530

    .

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,17 +1,146 @@
 import 'package:flutter/material.dart';
 
-class Register extends StatelessWidget {
-  const Register({super.key});
+class RegisterPage extends StatefulWidget {
+ const RegisterPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Register'),
-      ),
-      body: Center(
-        child: Text('Register Page'),
-      ),
-    );
-  }
+ @override
+ _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+ final _formKey = GlobalKey<FormState>();
+ final _firstNameController = TextEditingController();
+ final _lastNameController = TextEditingController();
+ final _ageController = TextEditingController();
+ final _emailController = TextEditingController();
+
+ @override
+ void dispose() {
+   _firstNameController.dispose();
+   _lastNameController.dispose();
+   _ageController.dispose();
+   _emailController.dispose();
+   super.dispose();
+ }
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     backgroundColor: const Color(0xFFF3D7D6),
+     body: Padding(
+       padding: const EdgeInsets.all(16.0),
+       child: Form(
+         key: _formKey,
+         child: ListView(
+           children: <Widget>[
+             const SizedBox(height: 60),
+             const Text(
+               'Register',
+               style: TextStyle(
+                 fontSize: 32,
+                 fontWeight: FontWeight.bold,
+                 color: Color(0xFF122C50),
+               ),
+               textAlign: TextAlign.center,
+             ),
+             const SizedBox(height: 140),
+             TextFormField(
+               controller: _firstNameController,
+               decoration: const InputDecoration(
+                 labelText: 'First Name *',
+                 filled: true,
+                 fillColor: Colors.white,
+                 border: OutlineInputBorder(
+                   borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                 ),
+               ),
+               validator: (value) {
+                 if (value == null || value.isEmpty) {
+                   return 'Please enter your first name';
+                 }
+                 return null;
+               },
+             ),
+             const SizedBox(height: 16),
+             TextFormField(
+               controller: _lastNameController,
+               decoration: const InputDecoration(
+                 labelText: 'Last Name *', 
+                 filled: true,
+                 fillColor: Colors.white,
+                 border: OutlineInputBorder(
+                   borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                 ),
+               ),
+               validator: (value) {
+                 if (value == null || value.isEmpty) {
+                   return 'Please enter your last name';
+                 }
+                 return null;
+               },
+             ),
+             const SizedBox(height: 16),
+             TextFormField(
+               controller: _ageController,
+               decoration: const InputDecoration(
+                 labelText: 'Age *',
+                 filled: true,
+                 fillColor: Colors.white,
+                 border: OutlineInputBorder(
+                   borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                 ),
+               ),
+               keyboardType: TextInputType.number,
+               validator: (value) {
+                 if (value == null || value.isEmpty) {
+                   return 'Please enter your age';
+                 }
+                 return null;
+               },
+             ),
+             const SizedBox(height: 16),
+             TextFormField(
+               controller: _emailController,
+               decoration: const InputDecoration(
+                 labelText: 'Email Address *',
+                 filled: true,
+                 fillColor: Colors.white,
+                 border: OutlineInputBorder(
+                   borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                 ),
+               ),
+               keyboardType: TextInputType.emailAddress,
+               validator: (value) {
+                 if (value == null || value.isEmpty) {
+                   return 'Please enter your email address';
+                 }
+                 if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                   return 'Please enter a valid email address';
+                 }
+                 return null;
+               },
+             ),
+             const SizedBox(height: 20),
+             ElevatedButton(
+               onPressed: () {
+                 if (_formKey.currentState!.validate()) {
+                   // Process data
+                 }
+               },
+               child: const Text('Submit'),
+               style: ElevatedButton.styleFrom(
+                 backgroundColor: const Color(0xFF122C50),
+                 foregroundColor: Colors.white,
+                 padding: const EdgeInsets.symmetric(vertical: 16),
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(8),
+                 ),
+               ),
+             ),
+           ],
+         ),
+       ),
+     ),
+   );
+ }
 }
--- lib/pages/rehab.dart
+++ lib/pages/rehab.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
-
-class Rehab extends StatelessWidget {
+import 'package:webview_flutter/webview_flutter.dart';
+class Rehab extends StatefulWidget {
   const Rehab({super.key});
 
   @override
+  _RehabState createState() => _RehabState();
+}
+
+class _RehabState extends State<Rehab> {
+  late WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://www.google.com/maps/search/rehabilitation+center/'));
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Rehab Page'),
-      ),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
